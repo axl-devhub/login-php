84c5fea5917f027970084e9d833fb729b0a962b9
Hideoka117 <123962472+Hideoka117@users.noreply.github.com>
2023-10-06 10:25:22 -0300

    Fixed db file

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2023 a las 14:23:24
+-- Tiempo de generación: 06-10-2023 a las 15:21:04
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `login_db`
 --
+CREATE DATABASE IF NOT EXISTS `login_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `login_db`;
 
 -- --------------------------------------------------------
 
